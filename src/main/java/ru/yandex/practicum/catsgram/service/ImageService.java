f19cb82afb946da22d7921e19fc484606d26d491
Lyudmila Rogovskaya <lyussenok@yandex.ru>
2025-08-09 14:53:58 +0700

    Спринт 11 - тема 2/5: Обработка запросов и формирование ответов - урок 6/8 Сохраняем изображения в Catsgram

--- src/main/java/ru/yandex/practicum/catsgram/service/ImageService.java
+++ src/main/java/ru/yandex/practicum/catsgram/service/ImageService.java
@@ -1,14 +1,12 @@
 package ru.yandex.practicum.catsgram.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 import ru.yandex.practicum.catsgram.exception.ConditionsNotMetException;
-import ru.yandex.practicum.catsgram.exception.ImageFileException;
-import ru.yandex.practicum.catsgram.exception.NotFoundException;
 import ru.yandex.practicum.catsgram.model.Image;
-import ru.yandex.practicum.catsgram.model.ImageData;
 import ru.yandex.practicum.catsgram.model.Post;
 
 import java.io.IOException;
@@ -28,31 +26,32 @@ public class ImageService {
 
     private final Map<Long, Image> images = new HashMap<>();
 
-    // Директория для хранения изображений
-    private String imageDirectory = "images";
+    // директория для хранения изображений
+    @Value("${catsgram.image-directory}")
+    private String imageDirectory;
 
-    // Сохранение списка изображений, связанных с указанным постом
+    // сохранение списка изображений, связанных с указанным постом
     public List<Image> saveImages(long postId, List<MultipartFile> files) {
         return files.stream().map(file -> saveImage(postId, file)).collect(Collectors.toList());
     }
 
-    // Сохранение отдельного изображения, связанного с указанным постом
-    public Image saveImage(long postId, MultipartFile file) {
+    // сохранение отдельного изображения, связанного с указанным постом
+    private Image saveImage(long postId, MultipartFile file) {
         Post post = postService.findById(postId)
                 .orElseThrow(() -> new ConditionsNotMetException("Указанный пост не найден"));
 
-        // Сохраняем изображение на диск и возвращаем путь к файлу
+        // сохраняем изображение на диск и возвращаем путь к файлу
         Path filePath = saveFile(file, post);
 
-        // Создаём объект для хранения данных изображения
+        // создаём объект для хранения данных изображения
         long imageId = getNextId();
 
-        // Создание объекта изображения и заполнение его данными
+        // создание объекта изображения и заполнение его данными
         Image image = new Image();
         image.setId(imageId);
         image.setFilePath(filePath.toString());
         image.setPostId(postId);
-        // запоминаем имя файла, которое было при его передаче
+        // запоминаем название файла, которое было при его передаче
         image.setOriginalFileName(file.getOriginalFilename());
 
         images.put(imageId, image);
@@ -60,62 +59,27 @@ public class ImageService {
         return image;
     }
 
-    // Получение данных об изображениях указанного поста
-    public List<Image> getPostImages(long postId) {
-        return images.values()
-                .stream()
-                .filter(image -> image.getPostId() == postId)
-                .collect(Collectors.toList());
-    }
-
-    // Загружаем данные указанного изображения с диска
-    public ImageData getImageData(long imageId) {
-        if (!images.containsKey(imageId)) {
-            throw new NotFoundException("Изображение с id = " + imageId + " не найдено");
-        }
-        Image image = images.get(imageId);
-        // Загрузка файла с диска
-        byte[] data = loadFile(image);
-
-        return new ImageData(data, image.getOriginalFileName());
-    }
-
-    // Сохранение файла изображения
+    // сохранение файла изображения
     private Path saveFile(MultipartFile file, Post post) {
         try {
-            // Формирование уникального имени файла на основе текущего времени и расширения оригинального файла
+            // формирование уникального названия файла на основе текущего времени и расширения оригинального файла
             String uniqueFileName = String.format("%d.%s", Instant.now().toEpochMilli(),
                     StringUtils.getFilenameExtension(file.getOriginalFilename()));
 
-            // Формирование пути для сохранения файла с учетом идентификаторов автора и поста
+            // формирование пути для сохранения файла с учётом идентификаторов автора и поста
             Path uploadPath = Paths.get(imageDirectory, String.valueOf(post.getAuthorId()), post.getId().toString());
             Path filePath = uploadPath.resolve(uniqueFileName);
 
-            // Создаём директории, если они еще не созданы
+            // создаём директории, если они ещё не созданы
             if (!Files.exists(uploadPath)) {
                 Files.createDirectories(uploadPath);
             }
 
-            // Сохраняем файл по сформированному пути
+            // сохраняем файл по сформированному пути
             file.transferTo(filePath);
             return filePath;
         } catch (IOException e) {
             throw new RuntimeException(e);
-        }
-    }
-
-    private byte[] loadFile(Image image) {
-        Path path = Paths.get(image.getFilePath());
-        if (Files.exists(path)) {
-            try {
-                return Files.readAllBytes(path);
-            } catch (IOException e) {
-                throw new ImageFileException("Ошибка чтения файла.  Id: " + image.getId()
-                        + ", name: " + image.getOriginalFileName(), e);
-            }
-        } else {
-            throw new ImageFileException("Файл не найден. Id: " + image.getId()
-                    + ", name: " + image.getOriginalFileName());
         }
     }
 
